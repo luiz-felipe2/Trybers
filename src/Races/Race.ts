b7da801783403bf68e166d910f4b184f7fe836b8
Luiz Felipe <luiz.felipe213@gmail.com>
2023-09-26 22:46:25 -0300

    feat: Req 1 ok

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,20 @@
-// iniciando.
+export default abstract class Race {
+  constructor(private _name:string, private _dexterity: number) {
+    this._name = _name;
+    this._dexterity = _dexterity;
+  }
+
+  static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints():number;
+}
